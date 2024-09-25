047bcf80be9b8e1f599dd7da23dbf22b1d4fc842
josegameplay12 <116831491+josegameplay12@users.noreply.github.com>
2024-09-25 11:52:27 -0300

    asfafasd

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 function soma(num1,num2) {
     return num1+num2
 }
-function areaqua(num1,num2){
+function multi(num1,num2){
     return num1*num2
 }
+function areaquadrado(lado){
+    return lado**2
+}
+let media= function (num1, num2, num3){
+    return (num1+num2+num3)/3
+}
 console.log(soma(3,4))
-console.log(areaqua(4,5))
+console.log(multi(4,5))
+console.log(areaquadrado(5))
+console.log(media(4,4,4))
