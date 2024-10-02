0e693b4b8fa78f9130ce29eb58048b95f7c2f19b
josegameplay12 <116831491+josegameplay12@users.noreply.github.com>
2024-10-02 11:42:10 -0300

    ...

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// alert('deu certo.. Wagner 39, Luiz 24')
 // function soma(num1,num2) {
 //     return num1+num2
 // }
@@ -8,21 +7,17 @@
 // function areaquadrado(lado){
 //     return lado**2
 // }
-// let media= function(num1, num2, num3){
+// let media= function (num1, num2, num3){
 //     return (num1+num2+num3)/3
 // }
-// let nome=prompt("Qual Média vc quer?")
-// document.write(media)
-// parseInt(prompt(media))
+// let nome=prompt("nome")
+// document.write(nome)
+// parseInt(prompt(''))
 // console.log(soma(3,4))
 // console.log(multi(4,5))
 // console.log(areaquadrado(5))
 // console.log(media(4,4,4))
+// parseInt(prompt(''))
+l
 
-let num1 = parseInt(prompt('numero'))
-let num2 = parseInt(prompt('numero'))
-let num3 = parseInt(prompt('numero'))
-function media(x,y,z){
-    return(x+y+z)/3
-}
-document.write(media(num1,num2,num3))
+let estados=['ac','al','ap','am','ba','ce','df','es','go','ma','mt','ms','mg','pa','pb','pr','pe','pi','rj','rn','rs','ro','rr','sc','sp','se','to']
