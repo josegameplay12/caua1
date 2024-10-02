2d2fbe32a61c7219d4a2e103a6a600becb645e1e
josegameplay12 <116831491+josegameplay12@users.noreply.github.com>
2024-10-02 11:45:31 -0300

    sssss

--- main.js
+++ main.js
@@ -18,6 +18,6 @@
 // console.log(areaquadrado(5))
 // console.log(media(4,4,4))
 // parseInt(prompt(''))
-l
+
 
 let estados=['ac','al','ap','am','ba','ce','df','es','go','ma','mt','ms','mg','pa','pb','pr','pe','pi','rj','rn','rs','ro','rr','sc','sp','se','to']
